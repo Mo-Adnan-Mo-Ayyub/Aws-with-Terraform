106b39d071a9f5d2c57281e1767a2e6e463c5247
Mo Adnan Mo Ayyub <moadnanmoayyub@gmail.com>
2025-12-08 03:07:31 +0530

    Completed Day 12

--- Day-12/backend.tf
+++ Day-12/backend.tf
@@ -5,6 +5,6 @@ terraform {
     key     = "dev/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true # Enable server-side encryption
-    #  use_lockfile = true # Built-in DynamoDB table for state locking in S3 backend
+    use_lockfile = true # Built-in DynamoDB table for state locking in S3 backend
   }
 }
